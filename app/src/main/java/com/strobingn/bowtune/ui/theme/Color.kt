8d963ce2953777f3bd1401938d8c51155f6e6ee4
Strobingn <120773065+Strobingn@users.noreply.github.com>
2026-09-14 18:39:22 -0400

    Add Color.kt

---
+++ app/src/main/java/com/strobingn/bowtune/ui/theme/Color.kt
@@ -0,0 +1,21 @@
+package com.strobingn.bowtune.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Forest / range-friendly palette (dark-theme friendly)
+val Green40 = Color(0xFF2E7D32)
+val Green80 = Color(0xFFA5D6A7)
+val Green90 = Color(0xFFC8E6C9)
+val Green20 = Color(0xFF1B5E20)
+
+val Amber40 = Color(0xFFF9A825)
+val Amber80 = Color(0xFFFFE082)
+
+val Teal40 = Color(0xFF00695C)
+val Teal80 = Color(0xFF80CBC4)
+
+val Grey10 = Color(0xFF1A1C1A)
+val Grey90 = Color(0xFFE2E3DE)
+val Grey99 = Color(0xFFFBFDF8)
+val Error40 = Color(0xFFBA1A1A)
+val Error80 = Color(0xFFFFB4AB)
